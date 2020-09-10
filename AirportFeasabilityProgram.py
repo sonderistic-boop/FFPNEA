885b8b78860ab813b5db3543ffcb67077ead74f0
sonderistic-boop <56941658+sonderistic-boop@users.noreply.github.com>
2020-09-10 09:56:04 +0100

    Add files via upload

--- AirportFeasabilityProgram.py
+++ AirportFeasabilityProgram.py
@@ -1,6 +1,25 @@
 import time
 import sys
 import re
+def setup(): 
+    verification1 = False
 
+
+setup()
+
+
+    
 print("\n-::Flight Feasability Program::-\n")
-option1 = input("1) Enter airport details \n2) Enter flight details \n3)Enter price plan and calculate profit \n4) Clear data \n5) Quit\n")
+
+
+
+while verification1 == False:
+    option1 = input("1) Enter airport details \n2) Enter flight details \n3)Enter price plan and calculate profit \n4) Clear data \n5) Quit\n")
+    if option1 != 1 or option1 != 2 or option1 != 3 or option1 != 4 or option1 != 5:
+        print("Invalid Input")
+    elif option1 == 1:
+        
+
+        
+
+        
